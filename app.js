b9621f0c795bd630d7c7bec43dcfeb36f0b7a0ad
Mateus Vieira <moniquefgvieira@gmail.com>
2025-07-04 17:49:16 -0500

    added let to variables

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ them to query specific positions within that string.
 const readline = require('readline-sync');
 
 //1. Use readlineSync.question() to prompt a user for input
-input = readline.question('Please enter the word or phrase you would like to index: ');
+let input = readline.question('Please enter the word or phrase you would like to index: ');
 
 //2. Prompt a user for an index number to find the character at that index
-indexValue = readline.questionInt('Please enter the index value you would like to return: ');
+let indexValue = readline.questionInt('Please enter the index value you would like to return: ');
 
 //3. Use bracket notation to access the character
-output = input[indexValue];
+let output = input[indexValue];
 
 //4. Print out the character
 console.log('The value of \'' + input + '\' at index ' + indexValue + ' is \'' + output + '\'');
